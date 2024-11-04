8c1ad6d3d8d3773945b326a4f058e3f3ff00f8cf
AshleyFlow <highflowey@gmail.com>
2024-11-04 14:37:39 +0330

    return actual lua thread

--- src/functions.rs
+++ src/functions.rs
@@ -4,18 +4,24 @@ use std::time::Duration;
 async fn lua_spawn(
     lua: mlua::Lua,
     (func, args): (mlua::Function, mlua::MultiValue),
-) -> mlua::Result<usize> {
+) -> mlua::Result<mlua::Thread> {
     let mut join_handles = lua.app_data_mut::<JoinHandles>().unwrap();
+
     let thread = lua.create_thread(func).unwrap();
+    let thread_inner = thread.clone();
 
     match thread.resume::<mlua::MultiValue>(args.clone()) {
         Ok(v) => {
             if v.get(0).is_some_and(is_poll_pending) {
                 join_handles.0.push(ThreadHandle {
                     tokio: Some(tokio::spawn(async move {
-                        let stream = thread.into_async::<()>(args);
-
-                        stream.await.expect("Failed to run spawned thread");
+                        match thread_inner.status() {
+                            mlua::ThreadStatus::Resumable => {
+                                let stream = thread_inner.into_async::<()>(args);
+                                stream.await.expect("Failed to run spawned thread");
+                            }
+                            _ => {}
+                        }
                     })),
                 });
             } else {
@@ -29,21 +35,7 @@ async fn lua_spawn(
         }
     };
 
-    Ok(join_handles.0.len())
-}
-
-async fn lua_cancel(lua: mlua::Lua, thread_id: usize) -> mlua::Result<()> {
-    let mut join_handles = lua.app_data_mut::<JoinHandles>().unwrap();
-    let handle = join_handles
-        .0
-        .get_mut(thread_id - 1)
-        .expect("Failed to get thread");
-
-    if let Some(tokio_handle) = handle.tokio.take() {
-        tokio_handle.abort();
-    }
-
-    Ok(())
+    Ok(thread)
 }
 
 async fn lua_wait(_lua: mlua::Lua, amount: f64) -> mlua::Result<()> {
@@ -66,8 +58,9 @@ impl Functions {
             .expect("Failed to create spawn function");
 
         let cancel = lua
-            .create_async_function(lua_cancel)
-            .expect("Failed to create cancel function");
+            .globals()
+            .get::<mlua::Table>("coroutine")?
+            .get::<mlua::Function>("close")?;
 
         let wait = lua
             .create_async_function(lua_wait)
