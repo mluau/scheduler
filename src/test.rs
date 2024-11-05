f4bf92c8616d99198788968031359cc21d5f1e2f
AshleyFlow <highflowey@gmail.com>
2024-11-05 17:17:56 +0330

    more tests

--- src/test.rs
+++ src/test.rs
@@ -1,47 +1,71 @@
 use std::time::Duration;
 use tokio::time::Instant;
 
-#[tokio::test(flavor = "multi_thread")]
-async fn test_spawn() {
-    let lua = mlua::Lua::new();
-    let task_functions = crate::functions::Functions::new(&lua).unwrap();
+macro_rules! create_test {
+    ($name:ident, $path:expr) => {
+        #[tokio::test(flavor = "multi_thread")]
+        async fn $name() {
+            let lua = mlua::Lua::new();
+            let task_functions = crate::functions::Functions::new(&lua).unwrap();
 
-    let task = lua.create_table().unwrap();
-    task.set("spawn", task_functions.spawn).unwrap();
-    task.set("defer", task_functions.defer).unwrap();
-    task.set("cancel", task_functions.cancel).unwrap();
-    task.set(
-        "wait",
-        lua.create_async_function(|_, secs: Option<f64>| async move {
-            let now = Instant::now();
-            tokio::time::sleep(Duration::from_secs_f64(secs.unwrap_or_default())).await;
-            Ok((Instant::now() - now).as_secs_f64())
-        })
-        .unwrap(),
-    )
-    .unwrap();
+            let task = lua.create_table().unwrap();
+            task.set("spawn", task_functions.spawn).unwrap();
+            task.set("defer", task_functions.defer).unwrap();
+            task.set("cancel", task_functions.cancel).unwrap();
+            task.set(
+                "wait",
+                lua.create_async_function(|_, secs: Option<f64>| async move {
+                    let now = Instant::now();
+                    tokio::time::sleep(Duration::from_secs_f64(secs.unwrap_or_default())).await;
+                    Ok((Instant::now() - now).as_secs_f64())
+                })
+                .unwrap(),
+            )
+            .unwrap();
+            task.set(
+                "delay",
+                lua.create_async_function(|lua, (secs, func): (f64, mlua::Function)| async move {
+                    tokio::time::sleep(Duration::from_secs_f64(secs)).await;
 
-    lua.globals().set("task", task).unwrap();
+                    crate::spawn_local(
+                        &lua,
+                        lua.create_thread(func).unwrap(),
+                        crate::SpawnProt::Spawn,
+                        (),
+                    )
+                    .await
+                    .expect("Failed to spawn thread");
 
-    crate::setup_scheduler(&lua);
+                    Ok(())
+                })
+                .unwrap(),
+            )
+            .unwrap();
 
-    let chunk = lua
-        .load(include_str!("../tests/spawn.luau"))
-        .set_name("./tests/spawn.luau");
+            lua.globals().set("task", task).unwrap();
 
-    crate::spawn_local(
-        &lua,
-        lua.create_thread(
-            chunk
-                .into_function()
-                .expect("Failed to turn chunk into function"),
-        )
-        .expect("Failed to turn function into thread"),
-        crate::SpawnProt::Spawn,
-        (),
-    )
-    .await
-    .expect("Failed to spawn thread");
+            crate::setup_scheduler(&lua);
 
-    assert!(crate::await_scheduler(&lua).await.errors.is_empty());
+            let chunk = lua.load(include_str!($path));
+
+            crate::spawn_local(
+                &lua,
+                lua.create_thread(
+                    chunk
+                        .into_function()
+                        .expect("Failed to turn chunk into function"),
+                )
+                .expect("Failed to turn function into thread"),
+                crate::SpawnProt::Spawn,
+                (),
+            )
+            .await
+            .expect("Failed to spawn thread");
+
+            assert!(crate::await_scheduler(&lua).await.errors.is_empty());
+        }
+    };
 }
+
+create_test!(spawn, "../tests/spawn.luau");
+create_test!(defer, "../tests/defer.luau");
