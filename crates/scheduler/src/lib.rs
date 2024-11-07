ed75f8e0dd2302b2cdbe4588cfb8a6149d97a220
AshleyFlow <highflowey@gmail.com>
2024-11-07 21:18:06 +0330

    make thread-safe

--- crates/scheduler/src/lib.rs
+++ crates/scheduler/src/lib.rs
@@ -20,23 +20,19 @@ fn spawn_future<T>(lua: &mlua::Lua, future: impl Future<Output = T> + Send + 'st
 where
     T: Send + 'static,
 {
-    let executor = {
-        let scheduler = lua.app_data_ref::<Scheduler>().unwrap();
-        Arc::clone(&scheduler.executor)
-    };
+    let scheduler = Arc::clone(&lua.app_data_ref::<Arc<Scheduler>>().unwrap());
 
-    executor.spawn(future)
+    scheduler.executor.spawn(future)
 }
 
 async fn yield_thread(lua: &mlua::Lua, thread: mlua::Thread) -> mlua::Result<mlua::MultiValue> {
-    let yield_pool = {
-        let scheduler = lua.app_data_ref::<Scheduler>().unwrap();
-        scheduler.yield_pool.0.clone()
-    };
+    let scheduler = Arc::clone(&lua.app_data_ref::<Arc<Scheduler>>().unwrap());
 
     let (sender, receiver) = channel::bounded(1);
 
-    yield_pool
+    scheduler
+        .yield_pool
+        .0
         .send((thread.to_pointer() as usize, sender))
         .await
         .into_lua_err()?;
@@ -56,17 +52,16 @@ fn spawn_thread<A: mlua::IntoLuaMulti>(
 
     let args = args.into_lua_multi(lua)?;
 
-    let spawn_pool = {
-        let scheduler = lua.app_data_ref::<Scheduler>().unwrap();
-        scheduler.spawn_pool.0.clone()
-    };
+    let scheduler = Arc::clone(&lua.app_data_ref::<Arc<Scheduler>>().unwrap());
 
     let thread_inner = thread.clone();
     let args_inner = args.clone();
     let thread_id = thread_inner.to_pointer() as _;
 
     spawn_future(&lua.clone(), async move {
-        spawn_pool
+        scheduler
+            .spawn_pool
+            .0
             .send((thread_id, ThreadInfo(thread_inner, args_inner)))
             .await
             .expect("Failed to send thread to scheduler")
@@ -82,16 +77,15 @@ fn spawn_thread<A: mlua::IntoLuaMulti>(
 }
 
 async fn await_thread(lua: &mlua::Lua, thread: mlua::Thread) -> mlua::Result<mlua::MultiValue> {
-    let result_pool = {
-        let scheduler = lua.app_data_ref::<Scheduler>().unwrap();
-        scheduler.result_pool.0.clone()
-    };
+    let scheduler = Arc::clone(&lua.app_data_ref::<Arc<Scheduler>>().unwrap());
 
     let thread_id = thread.to_pointer() as _;
     let (sender, receiver) = channel::bounded(1);
 
     spawn_future(&lua.clone(), async move {
-        result_pool
+        scheduler
+            .result_pool
+            .0
             .send((thread_id, sender))
             .await
             .expect("Failed to send thread to scheduler")
@@ -126,39 +120,32 @@ fn tick_thread(thread_info: &ThreadInfo) -> Option<mlua::Result<mlua::MultiValue
     }
 }
 
-pub async fn await_scheduler(lua: &mlua::Lua) -> mlua::Result<Scheduler> {
-    let (executor, spawn_pool, yield_pool, result_pool) = {
-        let scheduler = lua.app_data_ref::<Scheduler>().unwrap();
-        (
-            Arc::clone(&scheduler.executor),
-            scheduler.spawn_pool.1.clone(),
-            scheduler.yield_pool.1.clone(),
-            scheduler.result_pool.1.clone(),
-        )
-    };
+pub async fn await_scheduler(lua: &mlua::Lua) -> mlua::Result<Arc<Scheduler>> {
+    let scheduler = Arc::clone(&lua.app_data_ref::<Arc<Scheduler>>().unwrap());
 
     let mut threads: HashMap<usize, ThreadInfo> = HashMap::new();
-    let mut suspended_threads: HashMap<usize, channel::Sender<mlua::MultiValue>> = HashMap::new();
+    let mut thread_yield_senders: HashMap<usize, channel::Sender<mlua::MultiValue>> =
+        HashMap::new();
+    let mut thread_result_senders: HashMap<usize, channel::Sender<mlua::Result<mlua::MultiValue>>> =
+        HashMap::new();
 
     loop {
-        executor.try_tick();
+        scheduler.executor.try_tick();
 
-        while let Ok((thread_id, thread_info)) = spawn_pool.try_recv() {
-            if let Some(sender) = suspended_threads.remove(&thread_id) {
+        while let Ok((thread_id, thread_info)) = scheduler.spawn_pool.1.try_recv() {
+            if let Some(sender) = thread_yield_senders.remove(&thread_id) {
                 sender.send(thread_info.1.clone()).await.into_lua_err()?;
             }
 
             threads.insert(thread_id, thread_info);
         }
 
-        while let Ok((thread_id, sender)) = yield_pool.try_recv() {
+        while let Ok((thread_id, sender)) = scheduler.yield_pool.1.try_recv() {
             threads.remove(&thread_id);
-            suspended_threads.insert(thread_id, sender);
+            thread_yield_senders.insert(thread_id, sender);
         }
 
         let mut finished_threads: HashMap<usize, mlua::Result<mlua::MultiValue>> = HashMap::new();
-        let mut result_senders: HashMap<usize, channel::Sender<mlua::Result<mlua::MultiValue>>> =
-            HashMap::new();
 
         for (thread_id, thread_info) in &threads {
             if let Some(result) = tick_thread(&thread_info) {
@@ -167,28 +154,23 @@ pub async fn await_scheduler(lua: &mlua::Lua) -> mlua::Result<Scheduler> {
             };
         }
 
-        while let Ok((thread_id, sender)) = yield_pool.try_recv() {
-            threads.remove(&thread_id);
-            suspended_threads.insert(thread_id, sender);
-        }
-
-        while let Ok((thread_id, sender)) = result_pool.try_recv() {
-            result_senders.insert(thread_id, sender);
+        while let Ok((thread_id, sender)) = scheduler.result_pool.1.try_recv() {
+            thread_result_senders.insert(thread_id, sender);
         }
 
         for (thread_id, thread_result) in finished_threads {
-            if let Some(sender) = result_senders.remove(&thread_id) {
+            if let Some(sender) = thread_result_senders.remove(&thread_id) {
                 sender.send(thread_result).await.into_lua_err()?;
             }
 
             threads.remove(&thread_id);
         }
 
-        if executor.is_empty() & suspended_threads.is_empty() {
+        if scheduler.executor.is_empty() & thread_yield_senders.is_empty() {
             break;
         };
     }
 
-    lua.remove_app_data::<Scheduler>()
+    lua.remove_app_data::<Arc<Scheduler>>()
         .ok_or_else(|| mlua::Error::runtime("Scheduler not found in app data container"))
 }
--- crates/scheduler/src/scheduler.rs
+++ crates/scheduler/src/scheduler.rs
@@ -16,7 +16,7 @@ pub struct Scheduler {
         channel::Sender<(usize, channel::Sender<mlua::Result<mlua::MultiValue>>)>,
         channel::Receiver<(usize, channel::Sender<mlua::Result<mlua::MultiValue>>)>,
     ),
-    pub(crate) executor: Arc<Executor<'static>>,
+    pub(crate) executor: Executor<'static>,
 
     // pub(crate) threads: Arc<Mutex<Vec<mlua::Thread>>>,
     pub errors: Vec<mlua::Error>,
@@ -28,13 +28,13 @@ impl Scheduler {
             spawn_pool: channel::unbounded(),
             yield_pool: channel::unbounded(),
             result_pool: channel::unbounded(),
-            executor: Arc::new(Executor::new()),
+            executor: Executor::new(),
 
             errors: Default::default(),
         }
     }
 
     pub fn setup(self, lua: &mlua::Lua) {
-        lua.set_app_data(self);
+        lua.set_app_data(Arc::new(self));
     }
 }
