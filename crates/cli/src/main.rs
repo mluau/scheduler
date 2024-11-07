cdd9c37f81a446d25fea3f2091e70f8a44cf8a7e
AshleyFlow <highflowey@gmail.com>
2024-11-07 21:49:48 +0330

    await main thread

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,14 +1,14 @@
 use clap::Parser;
 use mlua_scheduler::{scheduler::Scheduler, traits::LuaSchedulerMethods};
 use smol::fs;
-use std::path::PathBuf;
+use std::{path::PathBuf, sync::Arc};
 
 #[derive(Debug, Parser)]
 struct Cli {
     path: PathBuf,
 }
 
-async fn spawn_script(lua: mlua::Lua, path: PathBuf) -> mlua::Result<()> {
+async fn spawn_script(lua: mlua::Lua, path: PathBuf) -> mlua::Result<mlua::Thread> {
     let chunk = lua
         .load(fs::read_to_string(&path).await?)
         .set_name(fs::canonicalize(&path).await?.to_string_lossy());
@@ -17,9 +17,7 @@ async fn spawn_script(lua: mlua::Lua, path: PathBuf) -> mlua::Result<()> {
         lua.create_thread(chunk.into_function()?)?,
         mlua_scheduler::SpawnProt::Spawn,
         (),
-    )?;
-
-    Ok(())
+    )
 }
 
 fn main() {
@@ -29,10 +27,15 @@ fn main() {
 
     mlua_task_std::inject_globals(&lua).unwrap();
 
-    smol::block_on(spawn_script(lua.clone(), cli.path)).expect("Failed to spawn script");
-    smol::block_on(scheduler.run()).expect("Failed to run scheduler");
+    let thread =
+        smol::block_on(spawn_script(lua.clone(), cli.path)).expect("Failed to spawn script");
 
-    if scheduler.errors.is_empty() {
+    smol::spawn(async move {
+        scheduler.run().await.expect("Scheduler failed");
+    })
+    .detach();
+
+    if smol::block_on(lua.await_thread(thread)).is_ok() {
         std::process::exit(0);
     } else {
         std::process::exit(1);
