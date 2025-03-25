e00755ae412e2bd1c1570d896f79f87a17211d93
Your Name <you@example.com>
2025-03-24 21:25:07 -0400

    add better thread limit mw

--- crates/ext/src/feedbacks.rs
+++ crates/ext/src/feedbacks.rs
@@ -158,46 +158,60 @@ impl SchedulerFeedback for ThreadTracker {
 }
 
 /// A thread limit middleware
-pub struct ThreadLimitMiddleware {
-    limit: XRefCell<usize>,
-    created: XRefCell<usize>,
+pub struct ThreadLimiter {
+    thread_limit: usize,
+    threads: std::cell::Cell<usize>,
 }
 
-impl ThreadLimitMiddleware {
-    /// Creates a new thread limit middleware
-    pub fn new(limit: usize) -> Self {
+impl ThreadLimiter {
+    pub fn new(thread_limit: usize) -> Self {
         Self {
-            limit: XRefCell::new(limit),
-            created: XRefCell::new(0),
+            thread_limit,
+            threads: std::cell::Cell::new(0),
         }
     }
 
-    /// Sets the thread limit
-    ///
-    /// Panics if called concurrently
-    pub fn set_limit(&self, limit: usize) {
-        let mut limit_g = self.limit.borrow_mut();
-        *limit_g = limit;
+    /// Returns the current number of threads recorded
+    /// by the thread limiter.
+    pub fn current_threads(&self) -> usize {
+        self.threads.get()
+    }
+
+    /// Returns the thread limit set for this thread limiter.
+    pub fn thread_limit(&self) -> usize {
+        self.thread_limit
+    }
+
+    fn on_thread_add_impl(&self) -> mlua::Result<()> {
+        let threads = self.threads.get();
+        if threads >= self.thread_limit {
+            return Err(mlua::Error::external("Thread limit reached"));
+        }
+
+        self.threads.set(threads + 1);
+
+        Ok(())
     }
 }
 
-impl ThreadAddMiddleware for ThreadLimitMiddleware {
+impl mlua_scheduler::taskmgr::SchedulerFeedback for ThreadLimiter {
     fn on_thread_add(
         &self,
         _label: &str,
         _creator: &mlua::Thread,
         _thread: &mlua::Thread,
     ) -> mlua::Result<()> {
-        let limit = *self.limit.borrow();
-        let mut created = self.created.borrow_mut();
-        *created += 1;
+        self.on_thread_add_impl()
+    }
+}
 
-        if *created > limit {
-            return Err(mlua::Error::RuntimeError(
-                "Thread limit reached".to_string(),
-            ));
-        }
-
-        Ok(())
+impl ThreadAddMiddleware for ThreadLimiter {
+    fn on_thread_add(
+        &self,
+        _label: &str,
+        _creator: &mlua::Thread,
+        _thread: &mlua::Thread,
+    ) -> mlua::Result<()> {
+        self.on_thread_add_impl()
     }
 }
